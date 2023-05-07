6ff5b36fef7a4b06fb28bf7d89110b4c13a22f44
Eric Walker <eric.walker@gmail.com>
2023-05-07 16:54:58 -0600

    Save work

--- frontend/cypress/e2e/landing.cy.ts
+++ frontend/cypress/e2e/landing.cy.ts
@@ -5,12 +5,12 @@ describe('/', () => {
   })
 })
 
-// describe('/skills', () => {
-//   beforeEach(() => {
-//     cy.visit('/skills')
-//   })
+describe('/skills', () => {
+  beforeEach(() => {
+    cy.visit('/skills')
+  })
 
-//   it('contains a listing of skills', () => {
-//     cy.get('[data-testid="page-name"]').should('contain', 'Skills')
-//   })
-// })
+  it('contains a listing of skills', () => {
+    cy.get('[data-testid="page-name"]').should('contain', 'Skills')
+  })
+})
